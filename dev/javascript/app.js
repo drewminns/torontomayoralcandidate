9c5b98c34384c215fc036484e2a3c9b530271a50
drewminns <dminns@gmail.com>
2014-04-29 17:22:33 -0400

    Updated

--- dev/javascript/app.js
+++ dev/javascript/app.js
@@ -11,7 +11,53 @@ candidateApp.factory('candidateFactory', function($http) {
 });
 
 candidateApp.controller('candidateCtrl', function($scope, candidateFactory) {
-	candidateFactory.getCandidatesAsync(function(results){
-		$scope.candidates = results.results;
+
+	candidateFactory.getCandidatesAsync(function(candidates){
+		$scope.candidates = candidates.results;
+		$scope.views = [];
+
+		$.each(candidates.results, function(index, element){
+			var view = element.views;
+			$.each(this.views, function(index, element){
+				var policy = element;
+				$scope.views.push(policy);
+				console.log(policy);
+			});
+		});
+
 	});
 });
+
+candidateApp.filter('searchFilter',function($filter) {
+	return function(items,searchfilter) {
+		var isSearchFilterEmpty = true;
+		angular.forEach(searchfilter, function(searchstring) {
+			if(searchstring !=null && searchstring !=""){
+				isSearchFilterEmpty= false;
+			}
+		});
+
+		if(!isSearchFilterEmpty){
+			var result = [];
+
+			angular.forEach(items, function(item) {
+				var isFound = false;
+				angular.forEach(item, function(term,key) {
+					if(term != null &&  !isFound){
+						term = term.toLowerCase();
+						angular.forEach(searchfilter, function(searchstring) {
+							searchstring = searchstring.toLowerCase();
+							if(searchstring !="" && term.indexOf(searchstring) !=-1 && !isFound){
+								result.push(item);
+								isFound = true;
+							}
+						});
+					}
+				});
+			});
+			return result;
+			}else{
+				return items;
+			}
+		}
+	});
