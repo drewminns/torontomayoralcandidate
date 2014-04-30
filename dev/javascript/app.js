8ae439b03a8d7171a7a9a3d623f166379f3c8f68
drewminns <dminns@gmail.com>
2014-04-29 22:32:50 -0400

    Updated Feed

--- dev/javascript/app.js
+++ dev/javascript/app.js
@@ -17,10 +17,9 @@ candidateApp.controller('candidateCtrl', function($scope, candidateFactory) {
 		$scope.views = [];
 
 		$.each(candidates.results, function(index, element){
-			var view = element.views;
+			var view = element;
 			$.each(this.views, function(index, element){
-				var policy = element;
-				$scope.views.push(policy);
+				var policy = element.name;
 				console.log(policy);
 			});
 		});
@@ -28,36 +27,5 @@ candidateApp.controller('candidateCtrl', function($scope, candidateFactory) {
 	});
 });
 
-candidateApp.filter('searchFilter',function($filter) {
-	return function(items,searchfilter) {
-		var isSearchFilterEmpty = true;
-		angular.forEach(searchfilter, function(searchstring) {
-			if(searchstring !=null && searchstring !=""){
-				isSearchFilterEmpty= false;
-			}
-		});
-
-		if(!isSearchFilterEmpty){
-			var result = [];
-
-			angular.forEach(items, function(item) {
-				var isFound = false;
-				angular.forEach(item, function(term,key) {
-					if(term != null &&  !isFound){
-						term = term.toLowerCase();
-						angular.forEach(searchfilter, function(searchstring) {
-							searchstring = searchstring.toLowerCase();
-							if(searchstring !="" && term.indexOf(searchstring) !=-1 && !isFound){
-								result.push(item);
-								isFound = true;
-							}
-						});
-					}
-				});
-			});
-			return result;
-			}else{
-				return items;
-			}
-		}
-	});
+// $scope.views.push(policy);
+// 				console.log(policy);
